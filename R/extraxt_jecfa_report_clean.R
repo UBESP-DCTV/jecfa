212c571fec40f40398b55ce8734789a66dcd9881
Corrado Lanera <corrado.lanera@gmail.com>
2024-08-04 07:49:16 +0200

    document extract_jecfa_report_clean

--- R/extraxt_jecfa_report_clean.R
+++ R/extraxt_jecfa_report_clean.R
@@ -1,3 +1,18 @@
+#' Extract JECFA report
+#'
+#' This function extracts and clean the JECFA report code.
+#'
+#' @param report (chr) The JECFA report code as reported in the JECFA
+#'   database
+#'
+#' @return (chr) The cleaned JECFA report code
+#' @export
+#'
+#' @examples
+#' compose_jecfa_list(1) |>
+#'   get_result() |>
+#'   dplyr::pull("Report1") |>
+#'   extract_jecfa_report_clean()
 extract_jecfa_report_clean <- function(report) {
   aux <- ifelse(
     substr(report, 1, 2) == '">',
