46a0ae3ee4f0f4fb0143d446337d46d2ac9720e8
Corrado Lanera <corrado.lanera@gmail.com>
2024-08-04 07:53:18 +0200

    refactor extract_jecfa_tox_abbr

--- R/extract_jecfa_tox_abbr.R
+++ R/extract_jecfa_tox_abbr.R
@@ -40,3 +40,16 @@ extract_jecfa_tox_abbr <- function(tox_monograph) {
 
   aux
 }
+
+
+extract_initial_part <- function(input_string) {
+  unlist(strsplit(input_string, "-"))[1]
+}
+
+extract_initial_part_bis <- function(input_string) {
+  unlist(strsplit(input_string, "/"))[1]
+}
+
+extract_initial_part_ter <- function(input_string) {
+  unlist(strsplit(input_string, "JECFA"))[1]
+}
