0f5706151cafb7b3d8d83d2e445c0af38b3fe565
Corrado Lanera <corrado.lanera@gmail.com>
2024-08-04 09:11:15 +0200

    document parse_pdf

--- R/parse_pdf.R
+++ R/parse_pdf.R
@@ -1,3 +1,12 @@
+#' Parse PDF
+#'
+#' This function parses a PDF file and returns the text.
+#'
+#' @param path Path to the PDF file
+#' @param dpi DPI to use for the OCR
+#'
+#' @return The text of the PDF file
+#' @export
 parse_pdf <- function(path, dpi = 150) {
   suppressMessages(
     pdftools::pdf_ocr_text(path, dpi = dpi)
